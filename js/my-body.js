e64309a36b000bbcf3f35f51afd6057d528afd05
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-06-05 08:08:47 -0500

    feat: :sparkles: funcionalidad del componente Quantity
    se realiza la logica de los botones para aumentar o disminuir el valor del quantity

--- js/my-body.js
+++ js/my-body.js
@@ -3,15 +3,42 @@ import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootst
 export class myBody extends HTMLElement{
     constructor(){
         super();
+        document.adoptedStyleSheets.push(styles);
+
     }
     async components(){
         return await (await fetch("view/my-body.html")).text();
     }
+    selection(e){
+        let $ = e.target;
+        if($.nodeName == "BUTTON"){
+            let inputs = document.querySelectorAll(`#${$.dataset.row} input`);
+            switch ($.innerHTML) {
+                case "-":
+                    inputs.forEach(element => {
+                        if(element.name == "Quantity" && element.value==0){
+                             document.querySelector(`#${$.dataset.row}`).remove();
+                        }else if(element.name == "Quantity"){
+                            element.value--;
+                       }
+                     });
+                break;
+                case "+":
+                    inputs.forEach(element => {
+                        if (element.name == "Quantity") {
+                            element.value++;
+                        }
+                    });
+                break;
+            }
+        }
+    }
     connectedCallback(){
-        document.adoptedStyleSheets.push(styles);
         this.components().then(html=>{
             this.innerHTML = html;
-        })
+            this.container = this.querySelector("#products");
+            this.container.addEventListener("click", this.selection);
+        });
     }
 }
 customElements.define('my-body',myBody);
